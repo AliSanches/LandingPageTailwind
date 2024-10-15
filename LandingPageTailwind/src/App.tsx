6c9cffa52eb3d25a12a9a9656e03dbf64fd0c819
Ali.San <alissonsanches54@gmail.com>
2024-10-15 17:45:42 -0300

    feat - CompleteLandingPageModel - frontend

--- LandingPageTailwind/src/App.tsx
+++ LandingPageTailwind/src/App.tsx
@@ -1,9 +1,23 @@
 import { Navbar } from "./components/Navbar";
+import { HeroSection } from "./components/HeroSection";
+import { FeatureSection } from "./components/FeatureSection";
+import { GameFlowSection } from "./components/GameFlowSection";
+import { Pricing } from "./components/Pricing";
+import { Testimonials } from "./components/Testimonials";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <GameFlowSection />
+        <Pricing />
+        <Testimonials />
+        <Footer />
+      </div>
     </>
   );
 }
